257870633124b94e22f2763823ac7e1499c0d60e
thomasbalzer <thomas.balzer16@gmail.com>
2022-08-08 18:24:45 -0600

    Update Teensy4SR.ino

--- Teensy4SR/Teensy4SR.ino
+++ Teensy4SR/Teensy4SR.ino
@@ -1,52 +1,33 @@
 #include <FlexCAN_T4.h>
-FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> Can2;
+FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_2> Can2;
 FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> Can1;
 
 //Define message from FlexCAN library
 static CAN_message_t txmsg2;
 static CAN_message_t rxmsg1;
 
-elapsedMicros RXTimer;
-elapsedMillis blinkTimer;
-elapsedMicros micro_counter;
-//Create a counter to keep track of message traffic
 uint32_t TXCount2 = 0;
 uint32_t RXCount1 = 0;
 
 //Define LED
 #define GREEN_LED_PIN 15
 #define RED_LED_PIN 14
-
 boolean GREEN_LED_state;
 boolean RED_LED_state;
 boolean LED_BUILTIN_state;
-
-uint32_t mask[4] = {0xFF, 0xFF00, 0xFF0000, 0xFF000000};
-uint8_t shift[4] = {0, 8, 16, 32};
-
-union u_seconds {
-  uint32_t count;
-  byte b[4];
-};
-
-// Declare the variable using the union
-u_seconds u_counter;
 
 //Define default baudrate
 #define BAUDRATE250K 250000
 
 String command;
 boolean toggle = false;
-boolean newData = false;
 
 void setup() {
-  //Set baudrate
-  Serial.begin(115200); delay(400);
+  Serial.begin(9600); delay(400);
   Can1.begin();
   Can1.setBaudRate(BAUDRATE250K);
   Can1.setMaxMB(16);
   Can1.enableFIFO(true);
-  Can1.mailboxStatus();
   Can1.enableFIFOInterrupt();
   Can1.onReceive(canSniff); // allows FIFO messages to be received in the supplied callback.
   Can2.begin();
@@ -63,39 +44,30 @@ void setup() {
 }
 
 void loop() {
-  Can1.events();
-
   if (Serial.available()) {
     command = Serial.readStringUntil('\n');
-
     if (command.equals("start")) {
       toggle = true;
     }
     else if (command.equals("stop")) {
+      digitalWrite(LED_BUILTIN, LOW);
       toggle = false;
+      delay(10);
+      Serial.printf("Messages Sent:%d\n", TXCount2);
+      Serial.printf("Messages Received:%d\n", RXCount1);
+      RXCount1 = 0;
+      TXCount2 = 0;
     }
   }
 
   if (toggle == true) {
-    u_counter.count = micro_counter;
-    for (int i = 0; i < 4; i++) {
-      txmsg2.buf[i] = u_counter.b[i];
+    if (Can2.getTXQueueCount() == 0) {
+      Can2.write(txmsg2);
+      TXCount2++;
     }
-
-    while (!Can2.write(txmsg2));
-    TXCount2++;
     LED_BUILTIN_state = !LED_BUILTIN_state;
     digitalWrite(LED_BUILTIN, LED_BUILTIN_state);
-    //Serial.printf("%08d: %02X %02X %02X %02X\n", txmsg2.id, txmsg2.buf[0], txmsg2.buf[1], txmsg2.buf[2], txmsg2.buf[3]);
-  }
-  if ((RXTimer > 100000) && newData == true) {
-    newData = false;
-    digitalWrite(GREEN_LED_PIN, LOW);
-    digitalWrite(LED_BUILTIN, LOW);
-    Serial.printf("Messages Sent:%d\n", TXCount2);
-    Serial.printf("Messages Received:%d\n", RXCount1);
-    RXCount1 = 0;
-    TXCount2 = 0;
+    //Serial.printf("%08d: %02X %02X %02X %02X\n", TXCount2,txmsg2.buf[0], txmsg2.buf[1], txmsg2.buf[2], txmsg2.buf[3]);
   }
 }
 
@@ -114,9 +86,5 @@ void printFrame(CAN_message_t rxmsg, uint8_t channel, uint32_t RXCount)
 
 void canSniff(const CAN_message_t &rxmsg1) {
   RXCount1++;
-  RXTimer = 0;
-  newData = true;
   //printFrame(rxmsg1, 1, RXCount1);
-  GREEN_LED_state = !GREEN_LED_state;
-  digitalWrite(GREEN_LED_PIN, GREEN_LED_state);
 }
